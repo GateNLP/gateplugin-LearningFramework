f1b3151350223d6e747bba6aa18e4cf0d3d84c99
Johann Petrak <johann.petrak@gmail.com>
2016-06-06 21:21:28 +0100

    Remove unnecessary import.

--- src/gate/plugin/learningframework/engines/EngineWekaExternal.java
+++ src/gate/plugin/learningframework/engines/EngineWekaExternal.java
@@ -19,7 +19,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import org.yaml.snakeyaml.Yaml;
