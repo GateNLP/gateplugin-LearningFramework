0c2f8f57c75d1364543ade365b22d4fd364b9d68
Johann Petrak <johann.petrak@gmail.com>
2017-11-24 16:28:56 +0000

    Start working on the Engine classes.

--- src/main/java/gate/plugin/learningframework/engines/EngineDV.java
+++ src/main/java/gate/plugin/learningframework/engines/EngineDV.java
@@ -8,18 +8,15 @@
  */
 package gate.plugin.learningframework.engines;
 
-import cc.mallet.types.Alphabet;
+import gate.AnnotationSet;
+import gate.plugin.learningframework.EvaluationMethod;
+import gate.plugin.learningframework.ModelApplication;
 import gate.plugin.learningframework.data.CorpusRepresentation;
-import gate.plugin.learningframework.data.CorpusRepresentationMallet;
-import gate.plugin.learningframework.data.CorpusRepresentationMalletSeq;
-import gate.plugin.learningframework.data.CorpusRepresentationMalletTarget;
 import gate.plugin.learningframework.features.FeatureInfo;
 import gate.plugin.learningframework.features.TargetType;
-import gate.plugin.learningframework.mallet.LFPipe;
-import gate.util.GateRuntimeException;
 import java.io.File;
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Common base class for all Engines which use the Dense Volatile representation
@@ -28,60 +25,5 @@ import java.util.ArrayList;
  * @author Johann Petrak
  */
 public abstract class EngineDV extends Engine {
-  
-  // TODO FROM HERE!!!
-  
-  protected CorpusRepresentationMallet corpusRepresentation; 
-
-  @Override
-  public CorpusRepresentation getCorpusRepresentation() {
-    return corpusRepresentation;
-  }
-  
-  protected void updateInfo() {
-    //System.err.println("In updateInfo, model is "+model);
-    if(model!=null) {
-      info.modelClass = model.getClass().getName();
-    }
-    info.nrTrainingInstances = corpusRepresentation.getRepresentationMallet().size();
-    info.nrTrainingDimensions = corpusRepresentation.getRepresentationMallet().getDataAlphabet().size();    
-    LFPipe pipe = (LFPipe)corpusRepresentation.getPipe();
-    Alphabet targetAlph = pipe.getTargetAlphabet();
-    if(targetAlph == null) {
-      info.nrTargetValues = 0;
-    } else {
-      info.nrTargetValues = targetAlph.size();
-      //info.classLabels = 
-      Object[] objs = targetAlph.toArray();
-      ArrayList<String> labels = new ArrayList<String>();
-      for(Object obj : objs) { labels.add(obj.toString()); }
-      info.classLabels = labels;
-    }
-    
-  }
-  
-  @Override
-  protected void saveCorpusRepresentation(File directory) {
-    corpusRepresentation.savePipe(directory);
-  }
-  
-  @Override
-  protected void loadAndSetCorpusRepresentation(URL directory) {
-    if(corpusRepresentation==null)
-      corpusRepresentation = CorpusRepresentationMalletTarget.load(directory);
-  }
-  
-  
-  @Override
-  protected void initWhenCreating(URL directory, Algorithm algorithm, String parameters, FeatureInfo fi, TargetType tt) {    
-    if(algorithm.getAlgorithmKind() == AlgorithmKind.SEQUENCE_TAGGER) {
-      corpusRepresentation = new CorpusRepresentationMalletSeq(fi, fi.getGlobalScalingMethod());
-    } else if(algorithm.getAlgorithmKind() == AlgorithmKind.REGRESSOR || 
-              algorithm.getAlgorithmKind() == AlgorithmKind.CLASSIFIER) {
-      corpusRepresentation = new CorpusRepresentationMalletTarget(fi, fi.getGlobalScalingMethod(), tt);
-    } else {
-      throw new GateRuntimeException("Not a usable algorithm kind for now with Mallet based engines: "+algorithm);
-    }
-  }
   
 }
---
+++ src/main/java/gate/plugin/learningframework/engines/EngineDVFileJson.java
@@ -0,0 +1,97 @@
+/*
+ *  Copyright (c) The University of Sheffield.
+ *
+ *  This file is free software, licensed under the 
+ *  GNU Library General Public License, Version 2.1, June 1991.
+ *  See the file LICENSE.txt that comes with this software.
+ *
+ */
+package gate.plugin.learningframework.engines;
+
+import gate.AnnotationSet;
+import gate.plugin.learningframework.EvaluationMethod;
+import gate.plugin.learningframework.ModelApplication;
+import gate.plugin.learningframework.data.CorpusRepresentation;
+import gate.plugin.learningframework.data.CorpusRepresentationVolatileDense2JsonStream;
+import gate.plugin.learningframework.features.FeatureInfo;
+import gate.plugin.learningframework.features.TargetType;
+import gate.util.Files;
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * Common base class for all Engines which are dense, volatile and write JSON to a file.
+ * 
+ * 
+ * @author Johann Petrak
+ */
+public abstract class EngineDVFileJson extends EngineDV {
+  
+  // For this engine, this will always be a CorpusRepresentationVolatileDense2JsonStream
+  protected CorpusRepresentationVolatileDense2JsonStream corpusRepresentation;
+  
+  // just as a reminder, we need to implement the following methods:
+  @Override
+  public CorpusRepresentation getCorpusRepresentation() { return corpusRepresentation; }
+  
+    
+  @Override
+  protected void initWhenCreating(URL directory, Algorithm algorithm, String parms, FeatureInfo featureInfo, TargetType targetType) {
+    File outDir = Files.fileFromURL(directory);
+    corpusRepresentation = new CorpusRepresentationVolatileDense2JsonStream(outDir, featureInfo);
+  }
+
+  @Override
+  protected void loadAndSetCorpusRepresentation(URL directory) {
+    File outDir = Files.fileFromURL(directory);
+    // We need to load the feature info from the directory as well, this should be 
+    // in its own serialized file for this kind of engine.
+    corpusRepresentation = new CorpusRepresentationVolatileDense2JsonStream(outDir, featureInfo);
+  }
+
+  @Override
+  protected void loadModel(URL directory, String parms) {
+    // TODO
+    
+    // TODO: how is this different from loadAndSetCorpusRepresentation, with regard to 
+    // timing and preconditions?
+  }
+
+  @Override
+  protected void saveCorpusRepresentation(File directory) {
+    // TODO
+  }
+
+  @Override
+  protected void saveModel(File directory) {
+    // TODO
+  }
+
+  @Override
+  public void trainModel(File dataDirectory, String instanceType, String parms) {
+    // TODO:
+  }
+
+  @Override
+  public EvaluationResult evaluate(String algorithmParameters, EvaluationMethod evaluationMethod, int numberOfFolds, double trainingFraction, int numberOfRepeats) {
+    throw new UnsupportedOperationException("Not supported (yet?)"); 
+  }
+
+  @Override
+  public List<ModelApplication> applyModel(AnnotationSet instanceAS, AnnotationSet inputAS, AnnotationSet sequenceAS, String parms) {
+    // TODO 
+    return null;
+  }
+
+  @Override
+  public void initializeAlgorithm(Algorithm algorithm, String parms) {
+    // TODO
+  }
+
+  
+  protected void updateInfo() {
+    // TODO:    
+  }
+  
+}
