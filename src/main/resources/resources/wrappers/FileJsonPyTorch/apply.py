478a7acc43f5b44c0aa037ff97fa43905d47cd93
Johann Petrak <johann.petrak@gmail.com>
2018-04-16 17:41:20 +0200

    Updated apply scripts.

--- src/main/resources/resources/wrappers/FileJsonPyTorch/apply.py
+++ src/main/resources/resources/wrappers/FileJsonPyTorch/apply.py
@@ -1,18 +1,45 @@
-from __future__ import print_function
 import sys
 import json
+import os
+import logging
+from gatelfdata import Dataset
+from gatelfpytorch import ModelWrapperSimple
+# from gatelfpytorch import ModelWrapper
 
-print("PYTHON, args=",sys.argv,file=sys.stderr)
+
+print("PYTHON APPLICATION SCRIPT, args=",sys.argv,file=sys.stderr)
+
+modelprefix=sys.argv[1]
+metafile=sys.argv[2]
+datadir=sys.argv[3]
+
+# Set up logging
+logger = logging.getLogger("gatelfdata")
+logger.setLevel(logging.ERROR)
+logger = logging.getLogger("gatelfpytorch")
+logger.setLevel(logging.DEBUG)
+streamhandler = logging.StreamHandler(stream=sys.stderr)
+formatter = logging.Formatter(
+                '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+streamhandler.setFormatter(formatter)
+logger.addHandler(streamhandler)
+filehandler = logging.FileHandler(os.path.join(datadir,"FileJsonPyTorch.train.log"))
+logger.addHandler(filehandler)
+
+# restore the wrapper
+wrapper = ModelWrapperSimple.load(modelprefix)
 
 with sys.stdin as infile:
     for line in infile:
-        print("PYTHON, input=",line,file=sys.stderr)
+        print("PYTHON APPLICATION, input=",line,file=sys.stderr)
         if line == "STOP":
             break
-        retmap = {}
-        retmap["status"] = "ok"
-        retmap["output"] = "theClass"
-        retmap["additional"] = 22
-        print(json.dumps(retmap))
+        # TODO: currently the LF sends individual instances here, we may want to change
+        # However we need to always apply to a set of instances, so wrap into another array
+        instancedata = json.loads(line)
+        preds=wrapper.apply([instancedata])
+        print("PYTHON APPLICATION, preds=", preds, file=sys.stderr)
+        print(preds)
+        # TODO: IMPORTANT!!! What the model returns is currently different from what the LF code expects!!!
         sys.stdout.flush()
-print("PYTHON: finishing",file=sys.stderr)
+print("PYTHON APPLICATION SCRIPT: finishing",file=sys.stderr)
--- src/main/resources/resources/wrappers/FileJsonPyTorch/apply.sh
+++ src/main/resources/resources/wrappers/FileJsonPyTorch/apply.sh
@@ -5,13 +5,44 @@ modelbase="$1"
 shift
 metafile="$1"
 shift
-datadir="$1"
+wrapperdir="$1"
 shift
+
+wrapperapply=$wrapperdir/apply.py
+
+datadir=`dirname modelbase`
+datadir=`cd $datadir; pwd -P`
+
+versionpython="UNKNOWN"
+wherepython=`which python`
+if [[ "x$wherepython" != "x" ]]
+then
+  versionpython=`python -V |& cut -f 2 -d " " | cut -f 1 -d'.'`
+fi
+if [[ "$versionpython" == "3" ]]
+then
+  pythoncmd=$wherepython
+else
+  wherepython=`which python3`
+  if [[ "x$wherepython" == "x" ]]
+  then
+    echo 'ERROR: could not find a python 3 interpreter, exiting'
+    exit 1
+  fi
+fi
+
+export PYTHONPATH="$wrapperdir/gate-lf-python-data:$wrapperdir/gate-lf-pytorch-wrapper"
+
 
 echo 'MODEL BASE NAME = ' $modelbase >&2
 echo 'META FILE       = ' $metafile  >&2
 echo 'DATA DIR        = ' $datadir   >&2
 echo 'ADDITIONALPARMS = ' "$@"       >&2
-echo 'RUNNING         = ' python3 "${datadir}"/apply.py "${modelbase}" "${metafile}" "${datadir}" "$@"  >&2
+echo 'WRAPPER SCRIPT  = ' $wrapperapply >&2
+echo 'ADDITIONALPARMS = ' "$@"       >&2
+echo 'PYTHON          = ' $wherepython >&2
+echo 'PYTHONPATH      = ' $PYTHONPATH >&2
+echo 'RUNNING         = ' ${wherepython} "${wrapperapply}" "${modelbase}" "${metafile}" "${datadir}" "$@"  >&2
 
-python3 "${datadir}"/apply.py "${modelbase}" "${metafile}" "${datadir}" "$@" 
+${wherepython} "${wrapperapply}" "${modelbase}" "${metafile}" "${datadir}" "$@" 
+
