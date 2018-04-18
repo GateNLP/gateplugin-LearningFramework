b2a45d106d10c3a9003a8f859c913982254e4762
Johann Petrak <johann.petrak@gmail.com>
2018-04-18 11:19:50 +0100

    Remove unused imports.

--- src/main/java/gate/plugin/learningframework/LF_Export.java
+++ src/main/java/gate/plugin/learningframework/LF_Export.java
@@ -41,12 +41,10 @@ import gate.plugin.learningframework.features.TargetType;
 import gate.util.Files;
 import gate.util.GateRuntimeException;
 import java.io.File;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.logging.Level;
 
 /**
  *
