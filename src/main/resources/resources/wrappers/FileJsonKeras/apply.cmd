f601d605a272358d45e40e7c551c23accd7b2afa
Johann Petrak <johann.petrak@gmail.com>
2019-01-15 12:14:19 +0000

    Add windows scripts for the keras backend

---
+++ src/main/resources/resources/wrappers/FileJsonKeras/apply.cmd
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+if "x%WRAPPER_HOME%"=="x" (
+  set WRAPPER_HOME=%~dp0
+)
+SET model=%1
+shift
+
+: create var with remaining arguments
+set r=%1
+:loop
+shift
+if "x%1"=="x" goto done
+set r=%r% %1
+goto loop
+:done
+
+if "x%PYTHON_BIN%"=="x" (
+  set PYTHON_BIN="%HOMEDRIVE%""%HOMEPATH%"\Miniconda3\python.exe
+)
+%PYTHON_BIN% %WRAPPER_HOME%\gate-lf-pytorch-json\apply.py %model% %r%
+
---
+++ src/main/resources/resources/wrappers/FileJsonKeras/train.cmd
@@ -0,0 +1,23 @@
+@ECHO OFF
+
+if "x%WRAPPER_HOME%"=="x" (
+  set WRAPPER_HOME=%~dp0
+)
+SET data=%1
+shift
+SET model=%1
+shift
+
+: create var with remaining arguments
+set r=%1
+:loop
+shift
+if "x%1"=="x" goto done
+set r=%r% %1
+goto loop
+:done
+
+if "x%PYTHON_BIN%"=="x" (
+  set PYTHON_BIN="%HOMEDRIVE%""%HOMEPATH%"\Miniconda3\python.exe
+)
+%PYTHON_BIN% %WRAPPER_HOME%\gate-lf-pytorch-json\train.py %data% %model% %r%
